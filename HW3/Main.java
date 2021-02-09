bb5e284bc95d8b487f14d16a38237746c1842f0a
Ksenia <ksenshaaa@gmail.com>
2021-02-09 18:30:27 +0300

    Added HW3

---
+++ HW3/Main.java
@@ -0,0 +1,43 @@
+package offline;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        //task 1
+        String[] words = {"dog", "bird", "cat", "elephant", "cat", "hippo", "tiger", "fox", "giraffe",
+        "fox", "dog", "rhino", "squirrel", "hippo", "lizard", "snake"};
+        ArrayList<String> wordsList = new ArrayList<>(Arrays.asList(words));
+        System.out.println(wordsList);
+        //without duplicates
+        System.out.println("Without duplicates:");
+        Set<String> stringSet = new HashSet<>(wordsList);
+        System.out.println(stringSet);
+        //count duplicates
+        System.out.println("Count duplicates");
+        System.out.println(countDuplicates(words));
+
+        //task2
+        TelephoneBook book = new TelephoneBook();
+        book.add("Ksenia", "88005553535");
+        book.add("Aleksandr", "12345678912");
+        book.add("Artem", "89997774456");
+        System.out.println(book.get("Ksenia"));
+        System.out.println(book.get("Aleksandr"));
+        System.out.println(book.get("Artem"));
+        System.out.println(book.get("Maria")); //было интересно, правильно ли будет работать, если спрошу о человеке, которого нет в книге
+    }
+
+    public static Map<String, Integer> countDuplicates(String[] words){
+        Map<String, Integer> mapWords = new HashMap<String, Integer>();
+        for(String word : words){
+            if(!mapWords.containsKey(word)){
+                mapWords.put(word, 1);
+            } else {
+                mapWords.put(word, mapWords.get(word) + 1);
+            }
+        }
+        return mapWords;
+    }
+}
